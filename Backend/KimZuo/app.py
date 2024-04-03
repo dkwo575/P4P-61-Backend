0ed246d82d98562d6d079a83358f4c77f247b5f5
kimzuo <109588821+yzuo500@users.noreply.github.com>
2024-04-03 14:30:03 +1300

    Add database.py function and connect to server

--- Backend/KimZuo/app.py
+++ Backend/KimZuo/app.py
@@ -1,18 +1,19 @@
 from flask import Flask, request
-import json
 import os
+from database import *
 
 app = Flask(__name__)
 current_directory = os.getcwd()
 database_directory = os.getcwd() + "/database"
+write_file_path = database_directory + "\\test.txt"
 
-# Set GET request through methods
-@app.route('/args', methods=["GET"])
+# Set GET POST request through methods
+@app.route('/args', methods=["GET", "POST"])
 def args_request():
-    # Receive and process GET data requests
+    # Receive and process GET POST data requests
     user_name = request.args.get('user_name')
     user_age = request.args.get('user_age')
 
     print("user_name = %s, user_age = %s" % (user_name, user_age))
-
+    newdata_join(user_name, user_age)
     return "user_name = %s, user_age = %s" % (user_name, user_age)
