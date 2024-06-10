1c5ea5f0f887c4c3116c4035003d992964fae837
dkwo575 <dkwo575@aucklanduni.ac.nz>
2024-06-10 13:59:14 +1200

    Add arduiono file for iot sensor
    update php file and arduino file for iot sensor

--- IOT_sensor/insert_data.php
+++ IOT_sensor/insert_data.php
@@ -3,8 +3,8 @@
 
 $servername = "localhost"; // server name(normally localhost
 $username = "DoJunKwon"; // user name
-$password = "smartfarm"; 
-$database_name = "sensor_db";
+$password = "smartfarm"; // replace your mysql password
+$database_name = "sensor_db"; // replace your datatbase name // sensor_db
 
 $conn = mysqli_connect($servername, $username, $password, $database_name);
 
@@ -15,17 +15,41 @@ if (!$conn) {
 
 echo "Database connection is fine!";
 
-if(isset($_POST ["temperature"]) && isset($_POST["humidity"]) ) {
+// Handle HTTP POST request from ESP32
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Extract sensor data (modify as needed)
+    $temperature = $_POST["temperature"];
+    $humidity = $_POST["humidity"];
+	$light = $_POST["light"];
+	$waterLevel = $_POST["WaterLevel"]; //isset($_POST["waterLevel"]) ? $_POST["waterLevel"]:'1'; 
+	$soilHumidity = $_POST["SoilHumidity"]; //isset($_POST["soilHumidity"]) ? $_POST["soilHumidity"]:'2';
+	$steam =  $_POST["Steam"]; // isset($_POST["steam"]) ? $_POST["steam"]:'3';
 	
-	$temp = $_POST ["temperature"];
+	 // Insert data into MySQL table
+    $sql = "INSERT INTO real_iot (temperature, humidity, light, waterLevel, soilHumidity, steam) VALUES ('$temperature', '$humidity', '$light', '$waterLevel', '$soilHumidity', '$steam')";
+	//$sql = "INSERT INTO environments (temperature, humidity, light, waterLevel, soilHumidity, steam) VALUES ('$temperature', '$humidity', '$light', '$waterLevel', '$soilHumidity', '$steam')"; change table name
+	if ($conn->query($sql) === TRUE) {
+        echo "Data inserted successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+
+/*
+if(isset($_POST["temperature"]) && isset($_POST["humidity"]) && isset($_POST["light"]) && isset($_POST["waterLevel"]) && isset($_POST["soilHumidity"]) && isset($_POST["steam"]) ) {
+	
+	$temp = $_POST["temperature"];
 	$humi = $_POST["humidity"];
-	//$light = $_POST["light"];
-	//$wLevel = $_POST["waterLevel"];
-	//$soilHumi = $_POST["soilHumidity"];
-	//$steam = $_POST["steam"];
+	$lightV = $_POST["light"];
+	$wLevel = $_POST["waterLevel"];
+	$soilHumi = $_POST["soilHumidity"];
+	$steamV = $_POST["steam"];
+	
 	// && isset($_POST["light"]) && isset($_POST["steam"])
 
-	$sql = "INSERT INTO environments (temperature, humidity) VALUES (".$temp.", ".$humi." )";
+	$sql = "INSERT INTO environments (temperature, humidity, light, waterLevel, soilHumidity, steam) VALUES (".$temp.", ".$humi.", ".$lightV.", ".$wLevel.", ".$soilHumi.", ".$steamV.")";
 //waterLevel, soilHumidity,   ".$wLevel.", ".$soilHumi.", , ".$light.", ".$steam." , light, steam
 	if (mysqli_query($conn, $sql)) {
 		echo "NEW record created successfully";
@@ -35,7 +59,6 @@ if(isset($_POST ["temperature"]) && isset($_POST["humidity"]) ) {
 	}
 }
 
-
-
+*/
 
 ?>
